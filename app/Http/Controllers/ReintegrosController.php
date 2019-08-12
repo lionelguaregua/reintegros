9548f5fbf1fb66f04e8bc626a9964fcdbebde118
quantico servicios <admin@interwelt-intl.com>
2019-08-12 17:39:32 -0400

    ultimo

--- app/Http/Controllers/ReintegrosController.php
+++ app/Http/Controllers/ReintegrosController.php
@@ -2026,7 +2026,7 @@ public function manualEvent(Request $request, $id)
 
      //  $files = Storage::allFiles('/public/solicitud/'.$id.'');
 
-         $files = Storage::allFiles('/');
+         $files = Storage::allFiles('/public/');
       
       dd($files);
     
